b5accd3873c2db3096ec83674e2ec124507fac17
Patrick Ail B. Bandola <96853751+bellefleur06@users.noreply.github.com>
2022-10-14 17:34:43 +0800

    initial-commit

---
+++ includes/script.js
@@ -0,0 +1,43 @@
+//Owl Carousel
+$('#facilities-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    dots:false,
+    nav:false,
+    smartSpeed:600,
+    autoplay:true,
+    autoplayTimeOut:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$('#reviews-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    nav:false,
+    smartSpeed:950,
+    autoplay:true,
+    autoplayTimeOut:4500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+});
